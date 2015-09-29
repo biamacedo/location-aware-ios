aafe67667904d6f91d5ff8325f176829f5b8ebe5
bianet13 <bia.macedo13@gmail.com>
2015-09-28 22:54:16 -0300

    init commit

--- Location Aware/ViewController.swift
+++ Location Aware/ViewController.swift
@@ -7,12 +7,95 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    var manager:CLLocationManager!
 
+    @IBOutlet weak var latitudeLabel: UILabel!
+    @IBOutlet weak var longiitudeLabel: UILabel!
+    @IBOutlet weak var courseLabel: UILabel!
+    @IBOutlet weak var speedLabel: UILabel!
+    @IBOutlet weak var altitudeLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        manager = CLLocationManager()
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+        
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let userLocation:CLLocation = locations[0]
+        
+        self.latitudeLabel.text = "\(userLocation.coordinate.latitude)"
+        self.longiitudeLabel.text = "\(userLocation.coordinate.longitude)"
+        
+        self.courseLabel.text = "\(userLocation.course)"
+        
+        self.speedLabel.text = "\(userLocation.speed)"
+        
+        self.altitudeLabel.text = "\(userLocation.altitude)"
+        
+        // Process of going from coordinates to an address
+        CLGeocoder().reverseGeocodeLocation(userLocation) { (placemarks, error) -> Void in
+            
+            if error != nil {
+                print(error)
+            } else {
+                
+                let p = CLPlacemark(placemark: placemarks![0])
+                
+                
+                var subThoroughfare:String = ""
+                var thoroughfare:String = ""
+                var subLocality:String = ""
+                var subAdministrativeArea:String = ""
+                var postalCode:String = ""
+                var country:String = ""
+
+                
+                if p.subThoroughfare != nil {
+                    subThoroughfare = p.subThoroughfare!
+                }
+                
+                if p.thoroughfare != nil {
+                    thoroughfare = p.thoroughfare!
+                }
+                
+                if p.subLocality != nil {
+                    subLocality = p.subLocality!
+                }
+                
+                if p.subAdministrativeArea != nil {
+                    subAdministrativeArea = p.subAdministrativeArea!
+                }
+                
+                if p.postalCode != nil {
+                    postalCode = p.postalCode!
+                }
+                
+                if p.country != nil {
+                    country = p.country!
+                }
+                
+                self.addressLabel.text = "\(subThoroughfare) \(thoroughfare) \n " +
+                "\(subLocality) \n \(subAdministrativeArea) \n \(postalCode) \n \(country)"
+                
+                
+            }
+            
+        }
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
